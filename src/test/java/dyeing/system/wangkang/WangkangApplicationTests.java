e9caa2085a9c718ec17f17df850b2805b35f963b
fishKang <707024922@qq.com>
2025-02-06 22:28:31 +0800

    染色系统框架初稿

---
+++ src/test/java/dyeing/system/wangkang/WangkangApplicationTests.java
@@ -0,0 +1,13 @@
+package dyeing.system.wangkang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangkangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
